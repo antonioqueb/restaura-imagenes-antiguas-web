d2dd89d2beb06e220bdec6f61146a6d70313b77b
Antonio Queb <92013606+antonioqueb@users.noreply.github.com>
2024-01-09 17:14:09 -0600

    Actualizar HomeTitle.jsx

--- components/HomeTitle/HomeTitle.jsx
+++ components/HomeTitle/HomeTitle.jsx
@@ -8,7 +8,7 @@ export const HomeTitle = () => {
   return (
   
         <header className={styles.HomeTitle__header}>
-          <h2 className={styles.HomeTitle__title}>Restaurar F  otos <span className={styles.HomeTitle__title__span}>Antiguas Online</span>
+          <h2 className={styles.HomeTitle__title}>Restaurar Fotos <span className={styles.HomeTitle__title__span}>Antiguas Online</span>
           </h2>
         </header>
   );
