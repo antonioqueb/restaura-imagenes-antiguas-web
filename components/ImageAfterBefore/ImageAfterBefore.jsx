32ecea1308dd6621d2efcff7c7d9be62c3b2d221
Antonio Queb <92013606+antonioqueb@users.noreply.github.com>
2024-01-11 17:03:51 -0600

    Refactor ImageAfterBefore component to use client-side rendering

--- components/ImageAfterBefore/ImageAfterBefore.jsx
+++ components/ImageAfterBefore/ImageAfterBefore.jsx
@@ -1,24 +1,25 @@
-import React, { useState, useCallback } from 'react';
+'use client';
+import { useState } from 'react';
 import Image from 'next/image';
 import styles from './ImageAfterBefore.module.css';
 
 export const ImageAfterBefore = () => {
   const [sliderPosition, setSliderPosition] = useState(50);
 
-  const handleMouseMove = useCallback((e) => {
+  const handleMouseMove = (e) => {
     const bounds = e.currentTarget.getBoundingClientRect();
     const position = ((e.clientX - bounds.left) / bounds.width) * 100;
     setSliderPosition(position);
-  }, []);
+  };
 
   return (
     <section className={styles.ImageAfterBefore__start} onMouseMove={handleMouseMove}>
       <div className={styles.ImageAfterBefore__beforeAfter} style={{ '--slider-position': sliderPosition + '%' }}>
-          <Image className={styles.ImageAfterBefore__image__before} src="/antes.png" alt="Antes" width={200} height={200} />
-          <Image className={styles.ImageAfterBefore__image__after} src="/despues.png" alt="Después" width={200} height={200} />
+          <Image className={styles.ImageAfterBefore__image__before} src="/antes.png" alt="Antes" width={400} height={400} />
+          <Image className={styles.ImageAfterBefore__image__after} src="/despues.png" alt="Después" width={400} height={400} />
           <div className={styles.ImageAfterBefore__slider} />
-      </div>
-    </section>
+        </div>
+      </section>
   );
 };
 
